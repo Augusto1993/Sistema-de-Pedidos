7a5b8e3a50932790aa91c7cdeb7f8da0e1fde53a
Augusto Mathias <luispiske1@gmail.com>
2018-03-30 20:27:49 -0300

    Estado e cidade

--- src/main/java/br/com/sistemadepedidos/SistemaDePedidosApplication.java
+++ src/main/java/br/com/sistemadepedidos/SistemaDePedidosApplication.java
@@ -1,8 +1,12 @@
 package br.com.sistemadepedidos;
 
 import br.com.sistemadepedidos.domain.Categoria;
+import br.com.sistemadepedidos.domain.Cidade;
+import br.com.sistemadepedidos.domain.Estado;
 import br.com.sistemadepedidos.domain.Produto;
 import br.com.sistemadepedidos.repositories.CategoriaRepository;
+import br.com.sistemadepedidos.repositories.CidadeRepository;
+import br.com.sistemadepedidos.repositories.EstadoRepository;
 import br.com.sistemadepedidos.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -17,6 +21,10 @@ public class SistemaDePedidosApplication implements CommandLineRunner {
     private CategoriaRepository categoriaRepository;
     @Autowired
     private ProdutoRepository produtoRepository;
+    @Autowired
+    private CidadeRepository cidadeRepository;
+    @Autowired
+    EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaDePedidosApplication.class, args);
@@ -41,7 +49,18 @@ public class SistemaDePedidosApplication implements CommandLineRunner {
         categoriaRepository.save(Arrays.asList(cat1,cat2));
         produtoRepository.save(Arrays.asList(p1,p2,p3));
 
+        Estado est1 = new Estado(null,"Rio Grande Do Sul");
+        Estado est2 = new Estado(null,"Santa Catarina");
 
+        Cidade c1 = new Cidade(null,"Pelotas",est1);
+        Cidade c2 = new Cidade(null, "Florianópolis",est2);
+        Cidade c3 = new Cidade(null,"Rio Grande",est1);
+
+        est1.getCidades().addAll(Arrays.asList(c1,c3));
+        est2.getCidades().addAll(Arrays.asList(c2));
+
+        estadoRepository.save(Arrays.asList(est1,est2));
+        cidadeRepository.save(Arrays.asList(c1,c2,c3));
 
 
 
