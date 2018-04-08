bba276562b31ce1dee5a5fdf3c5bfc709309e686
Grazziano Fagundes <grazzianofagundes@gmail.com>
2018-04-08 14:32:03 -0300

    Personalizando o retorno da validação

--- src/main/java/br/com/sistemadepedidos/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/sistemadepedidos/resources/exception/ResourceExceptionHandler.java
@@ -8,22 +8,34 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-    StandardError  err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
-
 
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandardError> dataIntegraty(DataIntegrityException e, HttpServletRequest request) {
-        StandardError  err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
 
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de Validação", System.currentTimeMillis());
+        /* Percorre a lista de erros e pra cada erro gera o obj fieldMessage */
+        for (FieldError x : e.getBindingResult().getFieldErrors()) {
+            err.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
 
 }
---
+++ src/main/java/br/com/sistemadepedidos/resources/exception/ValidationError.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistemadepedidos.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Grazziano Fagundes
+ */
+public class ValidationError extends StandardError {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Integer status, String msg, Long timeStamp) {
+        super(status, msg, timeStamp);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+
+}
