08137cd6f8e8febb55361e0ad1b0d84f4043ff36
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2018-04-17 16:55:45 -0300

    Criando o profile de teste

---
+++ src/main/java/br/com/sistemadepedidos/config/TestConfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistemadepedidos.config;
+
+import br.com.sistemadepedidos.services.DBService;
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ *
+ * @author grazz
+ */
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
