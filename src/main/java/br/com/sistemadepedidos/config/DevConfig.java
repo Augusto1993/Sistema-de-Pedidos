0521404d00b43fceda5244a57e930a6bd26f7ca3
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2018-04-20 17:05:03 -0300

    Criando o profile de desenvolvimento

---
+++ src/main/java/br/com/sistemadepedidos/config/DevConfig.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistemadepedidos.config;
+
+import br.com.sistemadepedidos.services.DBService;
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ *
+ * @author Grazziano Fagundes
+ */
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+    
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+        
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
