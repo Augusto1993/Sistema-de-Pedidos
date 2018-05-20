45472503561b3b488695ed68cb4c12c4e66f32bd
Augusto1993 <luispiske1@gmail.com>
2018-05-20 16:57:28 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/br/com/sistemadepedidos/config/TestConfig.java
+++ src/main/java/br/com/sistemadepedidos/config/TestConfig.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.sistemadepedidos.config;
 
 import br.com.sistemadepedidos.services.DBService;
 import java.text.ParseException;
+
+import br.com.sistemadepedidos.services.EmailService;
+import br.com.sistemadepedidos.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-/**
- *
- * @author grazz
- */
+
 @Configuration
 @Profile("test")
 public class TestConfig {
@@ -27,5 +22,10 @@ public class TestConfig {
     public boolean instantiateDatabase() throws ParseException {
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new MockEmailService();
     }
 }
